8e6c795bebbd00e8cada290d94e206df7c33c187
yangjiguang <yangjiguang@bigeyegroup.com>
2021-03-10 19:34:20 +0800

    删除pb文件

--- lib/Utils.go
+++ lib/Utils.go
@@ -45,6 +45,7 @@ func GetLocalIp() string { // {{{
 	addrs, _ := net.InterfaceAddrs()
 	var ip string = ""
 	for _, addr := range addrs {
+		//判断是否回环地址
 		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
 				return ipnet.IP.String()
